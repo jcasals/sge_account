024e905229695c814b7a4e8957c031ff1937a709
Jordi Casals <jcasals@iMac-de-Jordi.local>
2013-04-09 23:32:43 +0200

    0.1

--- header.php
+++ header.php
@@ -2,9 +2,13 @@
 	<div class="navbar-inner">
 		<a class="brand" href="index.php">SGE Accounting</a>
 		<ul class="nav pull-right">
-			<li>
-				<a href="#">About</a>
-			</li>
+			<li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a target="_blank" href="http://github.com/pelacables">Arnau</a></li>
+                    <li><a target="_blank" href="http://github.com/jcasals">Jordi</a></li>
+                </ul>
+            </li>
 		</ul>
 	</div>
 </div>
